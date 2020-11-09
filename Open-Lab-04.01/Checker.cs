c8b6d3b7e2c545616a16b2ae9415e4f2ccb77728
Jakub Dipčár <jakub.dipcar@gmail.com>
2020-11-09 01:31:13 +0100

    hah je pol druhej

--- Open-Lab-04.01/Checker.cs
+++ Open-Lab-04.01/Checker.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Principal;
 
 namespace Open_Lab_04._01
 {
@@ -6,7 +8,17 @@ namespace Open_Lab_04._01
     {
         public bool DoubleLetters(string str)
         {
-            throw new NotImplementedException();
+            int dlzka = str.Length;
+            for (int i = 0; i < dlzka - 1; i++)
+            {
+                char first = str[i];
+                char second = str[i+1];
+                if (first == second)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
